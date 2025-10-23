f8ec2f8022e60caccfd2ae42667394a66c2069ae
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-23 16:16:47 +0700

    tambahkan Box dan teks copyright di bagian bawah layar

--- app/src/main/java/com/example/myuipertama/UI Prodi.kt
+++ app/src/main/java/com/example/myuipertama/UI Prodi.kt
@@ -81,6 +81,20 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                stringResource(R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+}
+
 
 
 
