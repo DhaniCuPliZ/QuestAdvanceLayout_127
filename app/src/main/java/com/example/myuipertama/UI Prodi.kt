fbd6a17b7946d82431fbe440d0e1142a81409668
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-23 16:11:37 +0700

    tambahkan teks nama di dalam Column pada Card

--- app/src/main/java/com/example/myuipertama/UI Prodi.kt
+++ app/src/main/java/com/example/myuipertama/UI Prodi.kt
@@ -64,6 +64,10 @@ fun ActivitasPertama(modifier: Modifier){
                         .padding(all = 5.dp)
                 )
                 Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+
 
 
 
